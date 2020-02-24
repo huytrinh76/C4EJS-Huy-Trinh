39c227b32beb264567ade87aed09bc7f0562a814
huy trinh <thienthanh7622@gmail.com>
2020-02-24 22:22:19 +0700

    ly thuyet

---
+++ session 5/fix.js
@@ -0,0 +1,33 @@
+// function cal(a, b, oc){
+//     if(oc=='+'){
+//         return a+b
+//     }
+//     else if(oc=='-'){
+//         return a-b
+//     }
+//     else if(oc=='*'){
+//         return a*b
+//     }
+//     else if(oc=='/'){
+//         return a/b
+//     }
+// }
+
+// a=Math.floor(Math.random()*10)
+// b=Math.floor(Math.random()*10)
+// let oppoll=['+','-','*','/']
+// let oc=oppoll[Math.floor(Math.random()*4)]
+// if()
+// let c=cal(a,b,oc)
+// let numerpoll=[-1,0,1]
+// let wrongunit=numerpoll[Math.floor(Math.random()*3)]
+// let d=c+wrongunit
+// console.log(`${a} ${oc} ${b}=${d}`)
+
+
+// // if(check>0.5){
+// //     console.log(`${a}+${b}=${kq}`)
+// // }
+// // else if(kq+=1){
+// // console.log(`${a}+${b}=${kq}`)
+// // }
---
+++ session 5/java.js
@@ -0,0 +1,5 @@
+
+let t=0
+array=[2, 88, 7,9,10]
+tong=array.reduce(function(tong, sodt){return tong+sodt},t),
+console.log(tong)
