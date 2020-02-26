567e0c0bedb7de6e5ab32215d7b18f90ec54283a
Huy Trinh <thienthanh7622@gmail.com>
2020-02-26 21:56:53 +0700

    ly thuyet

---
+++ session 3/java.js
@@ -0,0 +1,53 @@
+// // // let monan1='pho'
+// // // let monan2='com'
+// // // let monan3='mi'
+// // // let monan4='bun'
+// // // let monan5='chao'
+
+// // let monan=['pho', 'com','mi','bun','chao','lau']
+// // console.log(monan)
+// // // a=prompt()
+// // // b=prompt()
+// // //monan[a]=b
+// // monan.splice(0, 1)
+// // monan[2]='trung'
+// // monan.push('sandwich')
+// // for(i=0;i<monan.length;i++){
+// // console.log(i, monan[i])
+// // }
+
+// let quanao=[
+//     'áo phông',
+//     'quần bò',
+//     'áo sơ mi',
+//     'áo có mũ',
+//     'quần đùi',
+//     'quần thể thao',
+// ]
+// console.log(quanao)
+// tuychon=prompt("Nhập lựa chọn của bạn vào đây (thêm, thay thế, đọc, xóa)").toLowerCase()
+// if(tuychon=="thêm"){
+//     themquanao=prompt("Thêm loại quần áo vào đây")
+//     quanao.push(themquanao)
+//     }
+// else if(tuychon=="thay thế"){
+// ttthaythe=Number(prompt("Nhập số thứ tự của loại quần áo bạn muốn thay thế")-1)
+// if(){
+    
+// }
+// else{
+// thaythe=prompt("Nhập loại quần áo bạn muốn thay thế")
+// quanao[ttthaythe]=thaythe
+// }
+// }
+// else if(tuychon=="xóa"){
+// xoaquanao=prompt("Nhập thứ tự bạn muốn xóa loại quần áo đó đi")
+// quanao.splice(xoaquanao, 1)
+// }
+
+// else{
+//     alert("Bạn nhập sai lựa chọn")
+// }
+// console.log("Danh sách sau khi thực hiện là")
+// for(i=0;i<quanao.length;i++){
+//     console.log(i+1, quanao[i])}
