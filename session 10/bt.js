c9ee97546731f1df287927a82888c1c6a1ad0ad4
huy trinh <thienthanh7622@gmail.com>
2020-05-07 22:13:40 +0700

    ss10

---
+++ session 10/bt.js
@@ -0,0 +1,34 @@
+async function pokemon(){
+    const pokemon= await fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
+    const data=await pokemon.json()
+    return data
+}
+
+
+
+
+async function render(){
+    const data= await pokemon()
+    const name=data.name
+    const weight=data.weight
+    const abilities=data.abilities
+    const types=data.types
+    const show=document.getElementById('show')
+    const html=`
+    <p>Name: ${name}</p>
+    <p>Weight: ${weight}</p>
+    `
+    show.insertAdjacentHTML('beforeend', html)
+    abilities.forEach(function(item){
+        show.insertAdjacentHTML('beforeend', `<li>${item.ability.name}</li>`)
+    })
+    abilities.forEach(function(item){
+        console.log(item.ability.name);
+        
+    })
+    types.forEach(function(item){
+        console.log(item.type.name);
+        
+    })
+}
+render()
