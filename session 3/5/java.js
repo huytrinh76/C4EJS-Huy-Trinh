4628e80a2426d52f74460257c66768395b85ef9e
Huy Trịnh <titaniummurdock@gmail.com>
2021-08-10 09:33:33 +0700

    MindX

--- session 3/5/java.js
+++
@@ -1,7 +0,0 @@
-let dayso=prompt("Nhập dãy số cần tính tổng vào đây ( mỗi số ngăn cách nhau bằng dấu cách)").split(' ')
-let sum=0
-console.log(dayso)
-for(i=0;i<dayso.length;i++){
-    sum+=Number(dayso[i])
-}
-console.log("Tổng các số trên là", sum)
--- session 5/ly thuyet/java.js
+++
@@ -1,5 +0,0 @@
-
-let t=0
-array=[2, 88, 7,9,10]
-tong=array.reduce(function(tong, sodt){return tong+sodt},t),
-console.log(tong)
